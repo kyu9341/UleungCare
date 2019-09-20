7c9429ec49740ac075b2e04ed361eb4f245da500
kyu9341 <kyu9341@naver.com>
2019-09-20 12:28:19 +0900

    Django 데이터베이스 연동 및 admin생성

---
+++ web/UleungCare/uleung_venv/Scripts/uleung/models.py
@@ -0,0 +1,15 @@
+
+from django.db import models
+
+# Create your models here.
+
+class HomeInfo(models.Model):
+    temperature = models.DecimalField(max_digits=10, decimal_places=10, verbose_name='온도')
+    humidity = models.DecimalField(max_digits=10, decimal_places=10,verbose_name='습도')
+    registered_dttm = models.DateTimeField(auto_now_add=True, verbose_name='측정시간')
+
+    class Meta:
+        db_table = 'Uleung_HomeInfo' # 테이블명 지정
+        verbose_name = '스마트 홈 데이터'
+        verbose_name_plural = '스마트 홈 데이터' # 복수형 표현도 설정
+
