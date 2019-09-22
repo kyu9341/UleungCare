6e18ab870bbeb3b817631471598eb16604e501a3
kyu9341 <kyu9341@naver.com>
2019-09-22 20:29:00 +0900

    Django admin 설정

--- web/UleungCare/uleung_venv/Scripts/uleung/admin.py
+++ web/UleungCare/uleung_venv/Scripts/uleung/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-
-# Register your models here.
+from .models import HomeInfo, AndroidRequested
 
 urlpatterns = [
     path('admin/', admin.site.urls)
 ]
+
+class HomeInfoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'temperature', 'humidity', 'registered_dttm') # user list 사용자명과 비밀번호를 확인할 수 있도록 구성
+
+class AndroidRequestedAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tvOnOff', 'airconOnOff', 'airconTempUp', 'airconTempDown') # user list 사용자명과 비밀번호를 확인할 수 있도록 구성
+
+
+admin.site.register(HomeInfo, HomeInfoAdmin) # 등록
+admin.site.register(AndroidRequested, AndroidRequestedAdmin) # 등록
+
--- web/UleungCare/uleung_venv/Scripts/uleung/views.py
+++ web/UleungCare/uleung_venv/Scripts/uleung/views.py
@@ -11,13 +11,27 @@ import json
 def androidControl(request):
 
     if request.method == 'GET':
-        return render(request, 'uleung/androidControl.html')
+        androidrequested = AndroidRequested.objects.order_by('id').last()
+        res_data={}
+        res_data['success'] = androidrequested.airconOnOff
+
+
+        return render(request, 'uleung/androidControl.html', res_data)
 
     elif request.method == 'POST':
         tvOnOff = request.POST.get('tvOnOff', None) # 템플릿에서 입력한 name필드에 있는 값을 키값으로 받아옴
         airconOnOff = request.POST.get('airconOnOff', None) # 받아온 키값에 값이 없는경우 None값으로 기본값으로 지정
         airconTempUp = request.POST.get('airconTempUp', None)
         airconTempDown = request.POST.get('airconTempDown', None)
+
+        #androidrequesteds = AndroidRequested.objects.all() #AndroidRequested에 있는 모든 객체를 불러와 androidrequesteds에 저장
+
+ #       if(int(tvOnOff) == 2): # 기본값을 수신한 경우 DB에 저장된 값으로 유지
+ #          tvOnOff = AndroidRequested.tvOnOff.last()
+
+   #     if(int(airconOnOff) == 2):
+  #          airconOnOff = AndroidRequested.airconOnOff.last()
+
 
         res_data = {} # 응답 메세지를 담을 변수(딕셔너리)
 
