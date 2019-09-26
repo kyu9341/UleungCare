bfe5335cfd59cdf159f55c5ae4e9d8f015359ae4
kyu9341 <kyu9341@naver.com>
2019-09-27 00:04:36 +0900

    Django setting.py secret_key 분리

--- web/UleungCare/uleung_venv/Scripts/UleungCare/UleungCare/settings.py
+++ web/UleungCare/uleung_venv/Scripts/UleungCare/UleungCare/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import json
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +22,21 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dr3rg4&@%cu$^0xc^9g8dqq99l5bziw^ib*a94!4zsf1)+25_='
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        print(secrets[setting])
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {0} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
+# SECRET_KEY = 'dr3rg4&@%cu$^0xc^9g8dqq99l5bziw^ib*a94!4zsf1)+25_='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
