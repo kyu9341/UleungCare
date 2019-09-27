fd86737ae5b9e0032e826e0bd504ebd6c91e777b
kyu9341 <kyu9341@naver.com>
2019-09-27 11:25:00 +0900

    setting

--- web/UleungCare/uleung_venv/Scripts/UleungCare/UleungCare/settings.py
+++ web/UleungCare/uleung_venv/Scripts/UleungCare/UleungCare/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1', 'ec2-18-223-126-253.us-east-2.compute.amazonaws.com']
 
 # Application definition
 
