25ff05ae1c908e10117a6863a8d7644b7e01d282
조승현 <cleanjjsh@naver.com>
2019-09-30 00:53:43 +0900

    arduino IR code
    기존 코드에서 좀더 향상된 코드로 업데이트 하였다
    인코딩 방식을 신경쓰지 않아도 원하는 데이터를 전송 할 수 있게 변경

--- arduino/IRrecve2/IRrecve2.ino
+++ arduino/IRrecve2/IRrecve2.ino
@@ -14,39 +14,25 @@ void  setup ( )
 void  dumpCode (decode_results *results)
 {
   // Start declaration
-  Serial.print("unsigned int  ");          // variable type
-  Serial.print("rawData[");                // array name
+  //Serial.print("unsigned int  ");          // variable type
+  //Serial.print("rawData[");                // array name
   Serial.print(results->rawlen - 1, DEC);  // array size
-  Serial.print("] = {");                   // Start declaration
+  Serial.print(",");
+  //Serial.print("] = {");                   // Start declaration
  
   // Dump data
   for (int i = 1;  i < results->rawlen;  i++) {
     Serial.print(results->rawbuf[i] * USECPERTICK, DEC);
     if ( i < results->rawlen-1 ) Serial.print(","); // ',' not needed on last one
-    if (!(i & 1))  Serial.print(" ");
+    //if (!(i & 1))  Serial.print(" ");
   }
  
   // End declaration
-  Serial.print("};");  // 
+  //Serial.print("};");  // 
  
   // Newline
   Serial.println("");
  
-  // Now dump "known" codes
-  if (results->decode_type != UNKNOWN) {
- 
-    // Some protocols have an address
-    if (results->decode_type == PANASONIC) {
-      Serial.print("unsigned int  addr = 0x");
-      Serial.print(results->address, HEX);
-      Serial.println(";");
-    }
- 
-    // All protocols have data
-    Serial.print("unsigned int  data = 0x");
-    Serial.print(results->value, HEX);
-    Serial.println(";");
-  }
 }
  
 //+=============================================================================
