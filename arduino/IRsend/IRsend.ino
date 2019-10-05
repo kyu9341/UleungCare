be6c9dd5cacd8bf4556ff5ee3ead22d4adbc79c2
조승현 <cleanjjsh@naver.com>
2019-10-06 02:38:33 +0900

    arduino IR data control
    아두이노에서 전송하는 IR 신호를 설정한다

--- arduino/IRsend/IRsend.ino
+++ arduino/IRsend/IRsend.ino
@@ -30,9 +30,38 @@ void setup()
 void loop() {
   int i;
   int khz = 38; // 38kHz carrier frequency for the NEC protocol
-  unsigned int irSignal[] = {4550,4500, 550,1650, 550,1700, 600,1650, 550,550, 550,600, 550,550, 550,600, 550,600, 500,1700, 550,1700, 550,1700, 550,600, 500,600, 500,600, 550,600, 500,600, 550,1700, 500,1750, 500,600, 550,1700, 550,600, 500,600, 500,650, 500,600, 500,600, 550,600, 500,1750, 500,600, 500,1750, 500,1750, 500,1750, 500,1700, 550}; //AnalysIR Batch Export (IRremote) - RAW
-  for(i=0;i<3;i++)
-    irsend.sendRaw(irSignal, sizeof(irSignal) / sizeof(irSignal[0]), khz); //Note the approach used to automatically calculate the size of the array.
+  unsigned int tvon[] ={4550,4500,550,1700,550,1650,550,1700,550,600,550,600,500,600,500,650,500,600,500,1750,500,1750,500,1700,550,600,500,600,550,600,500,600,500,650,500,600,500,1750,500,600,550,600,500,600,500,650,500,600,500,600,550,1700,550,600,500,1750,500,1700,550,1700,500,1750,500,1750,500,1750,500};
+  unsigned int volup[] ={4500,4500,550,1700,550,1700,550,1700,550,550,550,600,500,600,550,600,500,600,550,1700,500,1750,500,1750,500,600,500,650,500,600,500,600,550,600,500,1750,500,1750,500,1700,550,600,500,600,550,600,500,600,500,650,500,600,500,600,550,600,500,1750,500,1750,500,1700,550,1700,500,1750,500};
+  unsigned int voldown[] ={4500,4550,550,1650,550,1700,550,1700,550,600,500,600,550,600,500,600,550,600,500,1750,500,1700,550,1700,550,600,500,600,500,650,500,600,500,600,550,1700,500,1750,500,600,550,1700,550,600,500,600,500,650,500,600,500,600,550,600,500,1750,500,600,500,1750,500,1750,500,1750,500,1700,550};
+  unsigned int chup[] ={4500,4500,550,1700,550,1700,550,1700,550,550,550,600,500,600,550,600,500,600,550,1700,500,1750,500,1750,500,600,500,650,500,600,500,600,550,600,500,600,550,1700,500,650,500,600,500,1750,500,600,550,600,500,600,500,1750,500,600,550,1700,500,1750,500,600,550,1700,550,1700,500,1750,500};
+  unsigned int chdown[] ={4500,4500,550,1700,550,1700,550,1700,550,550,550,600,550,600,500,600,500,600,550,1700,550,1700,500,1750,500,600,550,600,500,600,550,600,500,600,500,650,500,600,500,600,550,600,500,1750,500,600,500,600,550,600,500,1750,500,1750,500,1700,550,1700,500,650,500,1750,500,1700,550,1700,500};
+  
+ 
+  for(i=0;i<3;i++){
+    irsend.sendRaw(tvon, sizeof(tvon) / sizeof(tvon[0]), khz); //Note the approach used to automatically calculate the size of the array.
+    Serial.print("send on\n");
+  }
+  delay(1000); //In this example, the signal will be repeated every 5 seconds, approximately.
 
-  delay(3000); //In this example, the signal will be repeated every 5 seconds, approximately.
+  for(i=0;i<3;i++){
+    irsend.sendRaw(volup, sizeof(volup) / sizeof(volup[0]), khz); //Note the approach used to automatically calculate the size of the array.
+    Serial.print("send vol up\n");
+  }
+  delay(1000); //In this example, the signal will be repeated every 5 seconds, approximately.
+  for(i=0;i<3;i++) {
+    irsend.sendRaw(voldown, sizeof(voldown) / sizeof(voldown[0]),khz ); //Note the approach used to automatically calculate the size of the array.
+    Serial.print("send vol down\n");
+   }
+   delay(1000); //In this example, the signal will be repeated every 5 seconds, approximately.
+  for(i=0;i<3;i++) {
+    irsend.sendRaw(chup, sizeof(chup) / sizeof(chup[0]),khz ); //Note the approach used to automatically calculate the size of the array.
+    Serial.print("send chdup\n");
+  }
+  delay(1000); //In this example, the signal will be repeated every 5 seconds, approximately.
+  for(i=0;i<3;i++) {
+    irsend.sendRaw(chdown, sizeof(chdown) / sizeof(chdown[0]),khz ); //Note the approach used to automatically calculate the size of the array.
+    Serial.print("send ch down\n");
+   }
+  
+  delay(1000); //In this example, the signal will be repeated every 5 seconds, approximately.
 }
