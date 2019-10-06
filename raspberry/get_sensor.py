8e66a57842f4d18ec64c497f534369385acdb0f0
조승현 <cleanjjsh@naver.com>
2019-10-06 18:06:50 +0900

    불필요 파일 제거 및 주석 추가

--- raspberry/get_sensor.py
+++ raspberry/get_sensor.py
@@ -1,6 +1,6 @@
 import serial
 
-def set_dev():
+def set_dev(): #라즈베리파이와 연결된 아두이노 시리얼 통신값 반환
 	try:
 		ser = serial.Serial('/dev/ttyACM0',9600)
 		ser.flushInput()
@@ -8,10 +8,10 @@ def set_dev():
 	except:
 		ser = serial.Serial('/dev/ttyACM1',9600)
 		ser.flushInput()
-                return ser
+        return ser
 
 
-def get_data(ser):
+def get_data(ser): # 연결된 아두이노의 센서값 수신후 반환
 	line = ser.readline()
         line = line.replace('\n','')
         data = line.split(',')
--- raspberry/uleungcare.py
+++ raspberry/uleungcare.py
@@ -1,17 +1,18 @@
-import get_sensor as sensor
-import control_device as condev
-import requests
-import json
+import get_sensor as sensor  # 아두이노와 데이터 통신
+
+import requests 
+import json # 서버에게 homeinfo를 보내고 remote_data를 받는다.
+
 import time
 from datetime import datetime
 
-class Home_info():
+class Home_info(): # home data class
     def __init__(self,temperature,light):
         self.temperature = temperature
         self.light = light
     def set_data(self,data1,data2):
-	self.temperature = data1
-	self.light = data2
+        self.temperature = data1
+        self.light = data2
 
     def set_temperature(slef,input_data):
         self.temperature = input_data
@@ -20,7 +21,7 @@ class Home_info():
         self.light = input_data
 
     def get_data(self):
-	return self.temperature,self.light
+        return self.temperature,self.light
 
     def get_temperature(self):
         return self.temperature
@@ -29,7 +30,7 @@ class Home_info():
         return self.light
 
 
-class Remote_info():
+class Remote_info(): # remote data super class
     def __init__(self,onoff,updown):
         self.onoff = onoff
         self.updown = updown
@@ -59,22 +60,28 @@ class Tv(Remote_info):
 class aircon(Remote_info):
     def __init__(self,thleshold,OnOff,UpDown):
         super().__init__(OnOff,UpDown)
-	self.thleshold = thleshold
+        self.thleshold = thleshold
 
 class Led():
     def __init__(self,thleshold,R,G,B):
         self.red = R
         self.green = G
         self.blue = B
-	self.thleshold = thleshold
+        self.thleshold = thleshold
 
 
     def set_rgb(self,rgb):
         red,green,blue = rgb
 
     def get_rgb(self):
-        rgb=(red,green,blue)
+        rgb=[self.red,self.green,self.blue]
         return rgb
+
+    def set_thleshold(self,thleshold):
+        self.thleshold = thleshold
+
+    def get_thleshold(self):
+        return self.thleshold
 
 
 #remote_data = {'tvOnOff':0,'airconOnOff':0,'tvChUpDown':0,'tvVolUpDown':0,'airconTempUpDown':0,'airconThleshold':0,'ledRed':0,'ledGreen':0,'ledBlue':0,'ledThleshold':0}
@@ -91,47 +98,51 @@ def remote_control(ser,order):
 
 
 def main():
-	while True:
+
+	while True: ## 아두이노 연결이 안될시 대기
 		try:
 			ser = sensor.set_dev()
 			break
 		except:
 			print('no device')
 			time.sleep(1)
-	home_obj = Home_info(0,0) ## home data obj 
-	past_remote_data = {}
 
-	while True:
+	home_obj = Home_info(0,0) ## home data obj 
+	past_remote_data = {} # 변경된 데이터가 있을 때 
+
+	while True: ## 아두이노 센서값 리딩 지연시간 대기
 		now = datetime.now()
 		data = sensor.get_data(ser) ## temperature, light
-		#data[1] /=10
+		
 		try:
 			home_obj.set_data(data[0],data[1])
 		except:
 			print('wait arduino')
 			continue
 
-		home_data = {'temperature':home_obj.get_temperature(),'light':home_obj.get_light(),'cctvURL':'http://192.168.0.146:8091'}
+		home_data = {'temperature':home_obj.get_temperature(),'light':home_obj.get_light(),'cctvURL':'http://192.168.0.146:8091'} # 서버에 전송할 home_data set
 
-		rs = requests.post('http://kyu9341.pythonanywhere.com/uleung/raspberry/',data=home_data)
-		new_remote_data  = rs.json()
+		rs = requests.post('http://kyu9341.pythonanywhere.com/uleung/raspberry/',data=home_data) # target server 
+		new_remote_data  = rs.json() # rs
 
-		new_rgb_led = [new_remote_data["ledRed"],new_remote_data["ledGreen"],new_remote_data["ledBlue"]]
+
+        
+		new_rgb_led = LED(ledThleshold,new_remote_data["ledRed"],new_remote_data["ledGreen"],new_remote_data["ledBlue"])
 
 		print(now)
 
-		try:
+        try:
 
-			if new_remote_data == past_remote_data:
+			if new_remote_data == past_remote_data: ## remote_data 변화 없을시 작동 안함
 	                        print("same")
 	                else:
 				print('data change')
-				if(new_rgb_led != past_rgb_led):
+				if(new_rgb_led.get_rgb() != past_rgb_led.get_rgb()): ## LED 제어
 					print("led change")
 					ser.write('1\n')
-					rgb_control(ser,new_rgb_led)
+					rgb_control(ser,new_rgb_led.get_rgb())
 
-				if(new_remote_data["tvOnOff"] != past_remote_data["tvOnOff"]):
+				if(new_remote_data["tvOnOff"] != past_remote_data["tvOnOff"]): ## TV 제어
 					print("tv on off")
 					ser.write('2\n')
 					remote_control(ser,1)
@@ -161,7 +172,7 @@ def main():
 						print("down")
                                                 remote_control(ser,3)
 
-				if(new_remote_data["airconTempUpDown"] != past_remote_data["airconTempUpDown"]):
+				if(new_remote_data["airconTempUpDown"] != past_remote_data["airconTempUpDown"]): ## 에어컨 제어
 					print("aircon temp up down")
 					ser.write('2\n')
 					if new_remote_data["airconTempUpDown"] > 0:
@@ -174,7 +185,7 @@ def main():
 
 			#'temperature':home_obj.get_temperature(),'light':home_obj.get_light()
 
-			if(new_remote_data["ledThreshold"] != 0 ):
+			if(new_remote_data["ledThreshold"] != 0 ): ## 조도값 사용 설정시 해당 조도값에 따른 LED 제어
 				if((new_remote_data["ledThreshold"] == 1) and (home_obj.get_light()<800)):
 					ser.write('1\n')
                                         rgb_control(ser,[0,0,0])
@@ -198,13 +209,13 @@ def main():
 
 
 
-			if(new_remote_data["airconThreshold"] != 0 ):
+			if(new_remote_data["airconThreshold"] != 0 ): ## 온도 자동제어시 온도에 따른 에어컨 제어
 				if(home_obj.get_temperature()>new_remote_data["airconThreshold"]):
 					remote_control(ser,7)
 				elif(home_obj.get_temperature()<new_remote_data["airconThreshold"]):
 					remote_control(ser,8)
 
-		except:
+        except:
 			print("no data")
 
 
@@ -217,12 +228,12 @@ def main():
 		#'airconThreshold'
 
 
-		for data in new_remote_data.items():
+        for data in new_remote_data.items():
 			print(data)
 
 
-		past_remote_data = new_remote_data
-		past_rgb_led = new_rgb_led
+        past_remote_data = new_remote_data ## 데이터 동기화
+        past_rgb_led = new_rgb_led
 
 
 main()
