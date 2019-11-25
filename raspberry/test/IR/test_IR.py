078558ec928984f6d64c2846fe3b26d40e357b57
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2019-11-25 19:36:08 +0900

    IR sensor data recive with python

--- raspberry/test/IR/test_IR.py
+++ raspberry/test/IR/test_IR.py
@@ -1,22 +1,27 @@
-import RPi.GPIO as GPIO
-from time import time
+import serial
+import time
+from datetime import datetime
 
-# Numbers GPIOs by physical location
-GPIO.setmode(GPIO.BOARD)
-# set pin 11 as an input pin with default as LOW v
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# define a function to acquire data
-def binary_acquire(pin, duration):
-    # acquires data as quickly as possible
-    t0 = time() # time is in seconds here
-    results = []
-    while (time() - t0) < duration:
-        results.append(GPIO.input(pin))
-    return results
 
-#print("Acquiring data for 1 second")
-#acquire data for 1 second
-results = binary_acquire(11, 1.0)
-print("Done!")
-print(",".join([str(result) for result in results]))
+def set_dev():
+        try:
+                ser = serial.Serial('/dev/ttyACM0',9600)
+                ser.flushInput()
+                return ser
+        except:
+                ser = serial.Serial('/dev/ttyACM1',9600)
+                ser.flushInput()
+        return ser
+
+
+while True:
+
+
+
+	ser = serial.Serial('/dev/ttyACM0',9600)
+  	ser.flushInput()
+
+
+	line = ser.readline()
+	print(line)
