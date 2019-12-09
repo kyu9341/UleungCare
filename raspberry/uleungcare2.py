fc778770637aa2a3c5a133a18ae635749852023f
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2019-12-09 21:44:26 +0900

    update uleungcare2.py

--- raspberry/uleungcare2.py
+++ raspberry/uleungcare2.py
@@ -1,5 +1,5 @@
 import get_sensor as sensor # communication with arduino
-
+import sys
 import requests #communication with web server
 import json
 
@@ -25,8 +25,8 @@ def main():
 			time.sleep(1)
 
 	home_data = [0,0] # home data [temperature,light]
-	past_remote_data = {} # check data chage 
-
+	past_remote_data = {} # check data chage
+	past_rgb_led ={'ledThreshold':0,'red':0,'green':0,'blue':0}
 	while True: # start main code
 
 		print('get home data')
@@ -45,23 +45,41 @@ def main():
 		rs = requests.post('http://kyu9341.pythonanywhere.com/uleung/raspberry/',data=home_data)
 
 		new_remote_data = rs.json() # recive new data
+		#('tvOnOff', 0)
+		#('airconOnOff', 0)
+		#('tvChUpDown', 0)
+		#('tvVolUpDown', 0)
+		#('airconTempUpDown', 0)
+		#('ledRed', 0)
+		#('ledGreen', 0)
+		#('ledBlue', 255)
+		#('ledThreshold', 1)
+		#('airconThreshold', 0)
 
 		new_rgb_led = {'ledThreshold':new_remote_data['ledThreshold'],'red':new_remote_data['ledRed'],'green':new_remote_data['ledGreen'],'blue':new_remote_data['ledBlue']}
 
 		print('time :',now)
 
-
 		try:
-			print('control...') # control code is here
 
-			# if data didn't change pass this part
+		# if data didn't change pass this part
 			if new_remote_data == past_remote_data:
 				print('same data')
 
 			else:
 				print('data change')
+				print('control...') # control code is here
+
+				if new_rgb_led != past_rgb_led:
+					print("led data change")
+					order = '1 '+str(new_rgb_led['red'])+' '+str(new_rgb_led['green'])+' '+str(new_rgb_led['blue'])
+					ser.write(order.encode())
+				if(new_remote_data['tvOnOff'] != past_remote_data['tvOnOff']): 
+					print('tv on off')
+
 		except:
-			print('no data')
+			es = sys.exc_info()[0]
+			print('no data',es)
 
 
 
@@ -70,5 +88,6 @@ def main():
 
 		past_remote_data = new_remote_data # data sync
 		past_rgb_led = new_rgb_led
+		print('\n')
 
 main()
