81881a9895ff277befa1a8ef98cebeed0bb729c4
조승현 <cleanjjsh@naver.com>
2019-12-10 19:32:35 +0900

    update IR regist and remove delay
    remove delay in arduino
    
    delay control in raspberrypi

--- arduino/arduino_test/Serial_recv/Serial_recv.ino
+++ arduino/arduino_test/Serial_recv/Serial_recv.ino
@@ -1,4 +1,5 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 void setup() {
   // put your setup code here, to run once:
  Serial.begin(9600);
@@ -6,16 +7,30 @@ void setup() {
 }
 
 void loop() {
+  
  char temp[100];
+ unsigned long IR_data;
   if(Serial.available()){
-    byte leng = Serial.readBytesUntil('g', temp, 20);
+    byte leng = Serial.readBytesUntil('g', temp, 30);
     
     Serial.print("Input data Lenght : ");
     Serial.println(leng);
+
+    IR_data = strtoul(temp,NULL,16);
     
     for(int i = 0; i < leng; i++){
       Serial.print(temp[i]);
     }
     Serial.println();
+    Serial.println(IR_data);
   }    
+/*
+int IR_data=0;
+  if(Serial.available()){
+    IR_data = Serial.parseInt();
+
+    Serial.println(IR_data);
+  }
+  */
+  
 }
--- arduino/uleungcare/uleungcare.ino
+++ arduino/uleungcare/uleungcare.ino
@@ -5,7 +5,7 @@
 int IRrecvPin = 2; // IR 수신 센서 포트
 IRrecv irrecv(IRrecvPin);
 IRsend irsend; 
-
+decode_results  results;        // Somewhere to store the results
 ////////////////////////////////////////////////
 
 //////////// sensor  //////////// 
@@ -16,7 +16,7 @@ int light_sensor = A1;
 
 const int ledPingreen = 8;
 const int ledPinyellow = 9;
-const int RED_PIN =7, GREEN_PIN= 6, BLUE_PIN = 5;
+const int RED_PIN =8, GREEN_PIN= 9, BLUE_PIN = 10;
 //const int RED_PIN =12, GREEN_PIN= 11, BLUE_PIN = 10;
 void setup() {
   Serial.begin(9600);
@@ -54,64 +54,50 @@ void RGB(int r, int g, int b){ //RGB 제어 함수
 ////////////////////       IR Senser          /////////////////////////////////
 
 ////////////////////       IR recive          /////////////////////////////////
-void  dumpCode (decode_results *results)
-{
-  // Start declaration
-  //Serial.print("unsigned int  ");          // variable type
-  //Serial.print("rawData[");                // array name
-  Serial.print(results->rawlen - 1, DEC);  // array size
-  Serial.print(",");
-  //Serial.print("] = {");                   // Start declaration
- 
-  // Dump data
-  for (int i = 1;  i < results->rawlen;  i++) {
-    Serial.print(results->rawbuf[i] * USECPERTICK, DEC);
-    if ( i < results->rawlen-1 ) Serial.print(","); // ',' not needed on last one
-    //if (!(i & 1))  Serial.print(" ");
-  }
- 
-  // End declaration
-  //Serial.print("};");  // 
- 
-  // Newline
-  Serial.println("");
- 
-}
+//void  IR_send(){}
 
 
 ////////////////////       IR Send         /////////////////////////////////
 
-void IR_send(int order) {
-  int i;
+void regist_IR() {
+  int pi_say;
+  while(true){
+    if (irrecv.decode(&results)){
 
-  if(order == 1){ // 라즈베리파이 시리얼 통신 값에 따른 아두이노 제어 
-       for(i=0;i<3;i++){
-        irsend.sendNEC(0xFFA55D,32); //Note the approach used to automatically calculate the size of the array.
-        Serial.println("send one");
-        }
-       delay(100);
-     }
-    if(order == 2){ // 라즈베리파이 시리얼 통신 값에 따른 아두이노 제어 
-      for(i=0;i<3;i++){
-        irsend.sendNEC(0xFFA66D,32); //Note the approach used to automatically calculate the size of the array.
-        Serial.println("send two");
+      if(results.decode_type > 0 && results.value != 0xFFFFFFFF){
+        Serial.print(results.decode_type);
+        Serial.print(',');
+        Serial.println(results.value,HEX);
+          /*
+           Serial.print("decode type : ");
+           Serial.println(results.decode_type);
+           Serial.print("bits : ");
+           Serial.println(results.bits);
+           Serial.println(results.value, HEX);
+
+         */
       }
-       delay(100);
-     }
-    if(order == 3){ // 라즈베리파이 시리얼 통신 값에 따른 아두이노 제어 
-      for(i=0;i<3;i++){
-        irsend.sendNEC(0xFFA22D,32); //Note the approach used to automatically calculate the size of the array.
-        Serial.println("send three");
+    
+      irrecv.resume();
+
+    }
+    
+    if(Serial.available()){ // 라즈베리파이 시리얼 값 수신
+      pi_say = Serial.parseInt();
+
+      if(pi_say == 2){ // 리모컨 등록 절차 종료 
+       //delay(100);
+       break;
+
       }
-       delay(100);
-     }
+    }
+  }
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
  
 void loop() {
   int i,r,g,b;
-  decode_results  results;        // Somewhere to store the results
  
   int tmp = analogRead(tmp_sensor);
   int light = analogRead(light_sensor);
@@ -121,30 +107,30 @@ void loop() {
   //float celsius = (voltage - 500) / 10.0; // 전압을 온도로 변환
   float celsius = (5.0*tmp*100.0)/1024.0;
    
-  Serial.print(celsius);
-  Serial.print(",");
-  Serial.print(light);
-  Serial.print("\n");//send data
 
-  
   if(Serial.available()){ // 라즈베리파이 시리얼 값 수신
     pi_say = Serial.parseInt();
 
+    if(pi_say == 3){//home data 읽기
+        Serial.print(celsius);
+        Serial.print(",");
+        Serial.println(light);
+        //Serial.print("\n");//send data
+    }
+    
     if(pi_say == 1){ // 라즈베리파이 시리얼 통신 값에 따른 아두이노 제어 
-       delay(100);
+       //delay(100);
       r = Serial.parseInt();
       g = Serial.parseInt();
       b = Serial.parseInt();
 
       RGB(r,g,b);
     }
-    if(pi_say == 2){
-      order = Serial.parseInt();
-      IR_send(order);
-
+    if(pi_say == 2){  // 리모컨값 등록 절차 시작
+       regist_IR();
     }
     
   }
-  delay(1000);
+  //delay(3000);
 
 }
